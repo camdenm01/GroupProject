a4363ed16076897a48638ab00f3ebd237c63c0c0
Camden <camdenmorgan01@gmail.com>
2021-10-21 11:21:09 -0400

    Added settings and credits activity, title and buttons to main screen

--- app/src/main/java/com/example/groupproject/MainActivity.java
+++ app/src/main/java/com/example/groupproject/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
+
+        Button startGameButton = (Button) findViewById(R.id.game_start_button);
+        Button settingsButton = (Button) findViewById(R.id.settings_button);
+        Button creditsButton = (Button) findViewById(R.id.credits_button);
+
+        startGameButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, GameActivity.class)));
+
+        settingsButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, SettingsActivity.class)));
+
+        creditsButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, CreditsActivity.class)));
     }
 }
