16b45b2f0da1e9eefcf58911c3f781dd37b52735
Camden <camdenmorgan01@gmail.com>
2021-12-13 14:21:04 -0500

    Added Tutorial Activity

--- app/src/main/java/com/example/groupproject/MainActivity.java
+++ app/src/main/java/com/example/groupproject/MainActivity.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,16 +15,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideTitleBar();
         setContentView(R.layout.activity_main);
 
         Button startGameButton = (Button) findViewById(R.id.game_start_button);
         Button settingsButton = (Button) findViewById(R.id.settings_button);
         Button creditsButton = (Button) findViewById(R.id.credits_button);
+        Button tutorialButton = (Button) findViewById(R.id.tutorial_button);
 
         startGameButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, GameActivity.class)));
 
         settingsButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, SettingsActivity.class)));
 
         creditsButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, CreditsActivity.class)));
+
+        tutorialButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, TutorialActivity.class)));
+    }
+
+    private void hideTitleBar(){
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.SOFT_INPUT_MASK_ADJUST);
     }
 }
